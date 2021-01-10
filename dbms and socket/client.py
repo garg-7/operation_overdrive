c5551e7a5ade6025067d2ab02f6e81cc7bdd4ffb
vyaskartik20 <vyas.2@iitj.ac.in>
2021-01-10 17:55:34 +0530

    update and delete operations added

--- dbms and socket/client.py
+++ dbms and socket/client.py
@@ -21,63 +21,37 @@ files = tuple(zip(files, userData))
 mydb = mysql.connector.connect(host="LAPTOP-RBAGRA85", user="root",passwd="letmepass", database="fileInfo")
 mycursor = mydb.cursor()
 
-
-#get credentials ::
-
-# import socket    
-# hostname = socket.gethostname()    
-# IPAddr = socket.gethostbyname(hostname)    
-# print("Your Computer Name is:" + hostname)    
-# print("Your Computer IP Address is:" + IPAddr)    
-
 # Your Computer Name is:LAPTOP-RBAGRA85
 # Your Computer IP Address is:192.168.56.1
 
-#new database creation ::
-
 # mycursor.execute("Create database fileInfo")
-
-#print all databases that exist ::
 
 # mycursor.execute("Show databases")
 # for db in mycursor:
 #     print (db)
 
-#Create a new table  ::
-
 # mycursor.execute("Create table backupData(file varchar(200), owner varchar(200))")
-
-#print all tables that exist ::
 
 # mycursor.execute("Show tables")
 # for tb in mycursor :
     # print(tb)
-
-#create entry in table ::
 
 dataPush = "Insert into backupdata(file, owner) values (%s, %s)"
 
 mycursor.executemany(dataPush,files)
 mydb.commit()
 
-
-    
-#delete table contencts  ::
-
-# deleteOperation = "DELETE FROM backupdata"
-
-# mycursor.execute(deleteOperation)
-# mydb.commit()
- 
-
-#print table contents ::
-
 mycursor.execute("Select * from backupdata")
 myFiles = mycursor.fetchall()
 
 for row in myFiles:
     print (row)
-    
-    
-# print(mydb)
 
+
+print(mydb)
+
+# import socket    
+# hostname = socket.gethostname()    
+# IPAddr = socket.gethostbyname(hostname)    
+# print("Your Computer Name is:" + hostname)    
+# print("Your Computer IP Address is:" + IPAddr)    
