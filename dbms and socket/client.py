cca45f34eb5deb4fe622b502ac1f9b5883a486db
vyaskartik20 <vyas.2@iitj.ac.in>
2021-01-17 09:32:13 +0530

    functions added

--- dbms and socket/client.py
+++ dbms and socket/client.py
@@ -4,104 +4,100 @@ import getpass
 import socket
 
 
+def initiateSocketConnection():
+    s = socket.socket()
+    host = input("Please enter the hostname of the server : ")
+    port = 9077
+    s.connect((host, port))
+    handleConnection(s)
 
-currentUserName = getpass.getuser()
-print(currentUserName)
-userData=[]
 
-files=os.listdir('backup')
-# print(files)
+def handleConnection(s):
+    print("Enter the password to authenticate the connection ! ")
+    passwordCheck = input()
 
-for file in (files):
-    userData.append(currentUserName)
+    s.send(passwordCheck.encode())
+    passwordVerified = s.recv(100).decode()
+
+    if(passwordVerified == "Correct") : 
+        print("Password authentication successful")
+    else : 
+        print("Password authentication unsuccessful")
+        print("The connection will be aborted")
+        exit()
+        
+    print("Enter Y if you want to receive a file or N if you want to participate in the file transfer")
+    purpose = input()
+    s.send(purpose.encode())
+
+
+def main():
+    currentUserName = getpass.getuser()
+    # print(currentUserName)
     
-files = tuple(zip(files, userData)) 
-# print(files)
-
-
-# s = socket.socket()
-# host = input("Please enter the hostname of the server : ")
-# port = 9077
-# s.connect((host, port))
-
-# print("Enter the password to authenticate the connection ! ")
-# passwordCheck = input()
-
-# s.send(passwordCheck.encode())
-# passwordVerified = s.recv(100).decode()
-
-# if(passwordVerified == "Correct") : 
-#     print("Password authentication successful")
-# else : 
-#     print("Password authentication unsuccessful")
-#     print("The connection will be aborted")
-#     exit()
+    mydb = mysql.connector.connect(host="LAPTOP-RBAGRA85", user="root",passwd="letmepass", database="fileInfo")
+    mycursor = mydb.cursor()
     
-# print("Enter Y if you want to receive a file or N if you want to participate in the file transfers")
-# purpose = input()
-# s.send(purpose.encode())
-
-
-
-
-mydb = mysql.connector.connect(host="LAPTOP-RBAGRA85", user="root",passwd="letmepass", database="fileInfo")
-mycursor = mydb.cursor()
+    # files = getFileInfo(currentUserName)
+    
+    # databaseCreation(mycursor)
+    
+    # printTables(mycursor)
+    
+    # createTableEntry(mycursor,mydb,files)
+    
+    # deteleTableData(mycursor,mydb)
+    
+    # printTableData(mycursor)
 
 #get credentials ::
+def getCredentials():
+    hostname = socket.gethostname()    
+    IPAddr = socket.gethostbyname(hostname)    
+    print("Your Computer Name is:" + hostname)    
+    print("Your Computer IP Address is:" + IPAddr)
 
-# import socket    
-# hostname = socket.gethostname()    
-# IPAddr = socket.gethostbyname(hostname)    
-# print("Your Computer Name is:" + hostname)    
-# print("Your Computer IP Address is:" + IPAddr)    
+def getFileInfo(currentUserName):
+    userData=[]
+    files=os.listdir('backup')
+    # print(files)
 
-# Your Computer Name is:LAPTOP-RBAGRA85
-# Your Computer IP Address is:192.168.56.1
-
-#new database creation ::
-
-# mycursor.execute("Create database fileInfo")
-
-#print all databases that exist ::
-
-# mycursor.execute("Show databases")
-# for db in mycursor:
-#     print (db)
-
-#Create a new table  ::
-
-# mycursor.execute("Create table backupData(file varchar(200), owner varchar(200))")
+    for file in (files):
+        userData.append(currentUserName)
+        
+    files = tuple(zip(files, userData))
+    return files 
 
 #print all tables that exist ::
-
-# mycursor.execute("Show tables")
-# for tb in mycursor :
-    # print(tb)
+def printTables(mycursor):
+    mycursor.execute("Show tables")
+    for tb in mycursor :
+        print(tb)
 
 #create entry in table ::
+def createTableEntry(mycursor,mydb,files):
+    dataPush = "Insert into backupdata(file, owner) values (%s, %s)"
+    mycursor.executemany(dataPush,files)
+    mydb.commit()
 
-# dataPush = "Insert into backupdata(file, owner) values (%s, %s)"
 
-# mycursor.executemany(dataPush,files)
-# mydb.commit()
-
-
-    
-#delete table contencts  ::
-
-# deleteOperation = "DELETE FROM backupdata"
-
-# mycursor.execute(deleteOperation)
-# mydb.commit()
+#delete table contents  ::
+def deteleTableData(mycursor,mydb) : 
+    deleteOperation = "DELETE FROM backupdata"
+    mycursor.execute(deleteOperation)
+    mydb.commit()
  
 
 #print table contents ::
+def printTableData(mycursor):
+    mycursor.execute("Select * from backupdata")
+    myFiles = mycursor.fetchall()
 
-mycursor.execute("Select * from backupdata")
-myFiles = mycursor.fetchall()
+    for row in myFiles:
+        print (row)
+    
+def printDatabaseName(mydb) :
+    print(mydb)
 
-for row in myFiles:
-    print (row)
-    
-    
-# print(mydb)
+if __name__ == '__main__' :
+    main()
--- dbms and socket/dbms.py
+++ dbms and socket/dbms.py
@@ -7,118 +7,135 @@ import getpass
 import socket
 from _thread import start_new_thread
 
+def initiateSocketConnection():
+    s = socket.socket()
+    port=9077
+    s.bind(('127.0.0.1',port))
+    s.listen() 
+    manageConnections(s)
 
-# def manageConnections ():
-#     while True:  
-#         clientsocket, address = s.accept()
-#         clients.append((clientsocket, address))
-#         start_new_thread(handleConnection, (clientsocket , address))
-#         print(f"Connected with {address}")
+def manageConnections (s):
+    clients = []
+    while True:  
+        clientsocket, address = s.accept()
+        clients.append((clientsocket, address))
+        start_new_thread(handleConnection, (clientsocket , address))
+        print(f"Connected with {address}")
 
 
-# def handleConnection(currentClient,address) :
-#     passwordCheck =  currentClient.recv(100).decode()
-#     if(passwordCheck == "letmepass"):
-#         print(f"Password authentication successful with {address}")
-#         currentClient.send(str("Correct").encode())
-#     else : 
-#         #server to be closed 
-#         print(f"Password authentication unsuccessful with {address}")
-#         currentClient.send(str("Wrong").encode())
+def handleConnection(currentClient,address) :
+    passwordCheck =  currentClient.recv(100).decode()
+    if(passwordCheck == "letmepass"):
+        print(f"Password authentication successful with {address}")
+        currentClient.send(str("Correct").encode())
+    else : 
+        #server to be closed 
+        print(f"Password authentication unsuccessful with {address}")
+        currentClient.send(str("Wrong").encode())
         
-#     purposeCheck =  currentClient.recv(100).decode()
+    purposeCheck =  currentClient.recv(100).decode()
     
-#     if(purposeCheck == 'Y'):
-#         print(f"File Transfer Requested by address : {address}")
-#     else :
-#         #do something
-#         print()        
+    if(purposeCheck == 'Y'):
+        print(f"File Transfer Requested by address : {address}")
+    else :
+        #do something
+        print()        
     
-
-currentUserName = getpass.getuser()
-# print(currentUserName)
-userData=[]
-
-files=os.listdir('backup')
-# print(files)
-
-for file in (files):
-    userData.append(currentUserName)
+def main():
+    currentUserName = getpass.getuser()
+    # print(currentUserName)
     
-files = tuple(zip(files, userData)) 
-# print(files)
+    mydb = mysql.connector.connect(host="LAPTOP-RBAGRA85", user="root",passwd="letmepass", database="fileInfo")
+    mycursor = mydb.cursor()
+    
+    initiateSocketConnection()
+    
+    # files = getFileInfo(currentUserName)
+    
+    # databaseCreation(mycursor)
+    
+    # printDatabases(mycursor)
+    
+    # printDatabaseName(mydb)
+    
+    # createTable(mycursor)
+    
+    # printTables(mycursor)
+    
+    # createTableEntry(mycursor,mydb,files)
+    
+    # deteleTableData(mycursor,mydb)
+    
+    # printTableData(mycursor)
 
-
-
-# s = socket.socket()
-# port=9077
-# s.bind(('127.0.0.1',port))
-# s.listen() 
-
-# clients = []
-# manageConnections()
-
-
-mydb = mysql.connector.connect(host="LAPTOP-RBAGRA85", user="root",passwd="letmepass", database="fileInfo")
-mycursor = mydb.cursor()
 
 
 #get credentials ::
+def getCredentials():
+    hostname = socket.gethostname()    
+    IPAddr = socket.gethostbyname(hostname)    
+    print("Your Computer Name is:" + hostname)    
+    print("Your Computer IP Address is:" + IPAddr)    
 
-# import socket    
-# hostname = socket.gethostname()    
-# IPAddr = socket.gethostbyname(hostname)    
-# print("Your Computer Name is:" + hostname)    
-# print("Your Computer IP Address is:" + IPAddr)    
+    # Your Computer Name is:LAPTOP-RBAGRA85
+    # Your Computer IP Address is:192.168.56.1
 
-# Your Computer Name is:LAPTOP-RBAGRA85
-# Your Computer IP Address is:192.168.56.1
+def getFileInfo(currentUserName):
+    userData=[]
+    files=os.listdir('backup')
+    # print(files)
+
+    for file in (files):
+        userData.append(currentUserName)
+        
+    files = tuple(zip(files, userData))
+    return files 
 
 #new database creation ::
+def databaseCreation(mycursor):
+    mycursor.execute("Create database fileInfo")
 
-# mycursor.execute("Create database fileInfo")
-
-#print all databases that exist ::
-
-# mycursor.execute("Show databases")
-# for db in mycursor:
-#     print (db)
+# print all databases that exist ::
+def printDatabases(mycursor):
+    mycursor.execute("Show databases")
+    for db in mycursor:
+        print (db)
 
 #Create a new table  ::
+def createTable(mycursor):
+    mycursor.execute("Create table backupData(file varchar(200), owner varchar(200))")
 
-# mycursor.execute("Create table backupData(file varchar(200), owner varchar(200))")
 
 #print all tables that exist ::
-
-# mycursor.execute("Show tables")
-# for tb in mycursor :
-    # print(tb)
+def printTables(mycursor):
+    mycursor.execute("Show tables")
+    for tb in mycursor :
+        print(tb)
 
 #create entry in table ::
+def createTableEntry(mycursor,mydb,files):
+    dataPush = "Insert into backupdata(file, owner) values (%s, %s)"
+    mycursor.executemany(dataPush,files)
+    mydb.commit()
 
-dataPush = "Insert into backupdata(file, owner) values (%s, %s)"
 
-mycursor.executemany(dataPush,files)
-mydb.commit()
-
-
-    
 #delete table contents  ::
-
-# deleteOperation = "DELETE FROM backupdata"
-
-# mycursor.execute(deleteOperation)
-# mydb.commit()
+def deteleTableData(mycursor,mydb) : 
+    deleteOperation = "DELETE FROM backupdata"
+    mycursor.execute(deleteOperation)
+    mydb.commit()
  
 
 #print table contents ::
+def printTableData(mycursor):
+    mycursor.execute("Select * from backupdata")
+    myFiles = mycursor.fetchall()
 
-mycursor.execute("Select * from backupdata")
-myFiles = mycursor.fetchall()
-
-for row in myFiles:
-    print (row)
+    for row in myFiles:
+        print (row)
     
-    
-# print(mydb)
+def printDatabaseName(mydb) :
+    print(mydb)
 
+if __name__ == '__main__' :
+    main()
