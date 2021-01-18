ba5f48d4f386edd84fe56d52d2b120242da85e9f
Devin Garg <garg.7@iitj.ac.in>
2021-01-18 13:21:57 +0530

    separated mic & speaker sockets

--- audio/client.py
+++ audio/client.py
@@ -10,17 +10,30 @@ import pyaudio
 class frame_keeper:
     def __init__(self):
         self.frames = []
+        self.frames_save = []
 
-def play_audio(f, stream):
+def play_audio(f, f1, speaker_stream, mic_stream):
     last_time = time.time()
     while time.time() - last_time < 5:
         try:
             data_to_be_played = f.frames[0]
-            stream.write(data_to_be_played)
+            speaker_stream.write(data_to_be_played)
+            data_to_be_played = f1.frames[0]
+            if max(data_to_be_played)>100:
+                mic_stream.write(data_to_be_played)
             f.frames.pop(0)
+            f1.frames.pop(0)
             last_time = time.time()
         except IndexError:
             pass
+
+def receiving_data(s, f):
+    while True:
+        recv_data = s.recv(4129)
+        f.frames.append(recv_data)
+        f.frames_save.append(recv_data)
+        if len(recv_data)==0:
+            break
 
 def main():
     CHUNK = 1024
@@ -31,8 +44,7 @@ def main():
     shost = input("Enter server IP")
     sport = 11112
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    s.connect((shost, sport))
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     f = frame_keeper()
     f1 = frame_keeper()
     p = pyaudio.PyAudio()
@@ -45,27 +57,34 @@ def main():
         print(i['index'], i['name'])
     speaker_id = int(input('Enter index of speaker to use: #'))
     print(type(speaker_id))
-    stream = p.open(format=format,
+    speaker_stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     output=True,
                     output_device_index=speaker_id)
-    t = Thread(target=play_audio, args=(f, stream,))
-    t1 = Thread(target=play_audio, args=(f1, stream,))
+    
+    mic_stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    output=True,
+                    output_device_index=speaker_id)
+
+    t = Thread(target=play_audio, args=(f, f1, speaker_stream, mic_stream))
     t.start()
-    t1.start()
-    while True:
-        recv_data = s.recv(3000)
-        recv_data = pickle.loads(recv_data)
-        speaker_data = recv_data['speaker']
-        mic_data = recv_data['mic']
-        f.frames.append(speaker_data)
-        f1.frames.append(mic_data)
-        if len(recv_data)==0:
-            break
+    s.connect((shost, sport))
+    speaker_data_thread = Thread(target=receiving_data, args=(s, f))
+    speaker_data_thread.start()
+
+    s1.connect((shost, sport))
+    mic_data_thread = Thread(target=receiving_data, args=(s1, f1))
+    mic_data_thread.start()
+    
+    speaker_data_thread.join()
+    mic_data_thread.join()
     print('Finished receiving audio stream.')
     # print('Saving recording as wav file.')
-    # multi.save_wav(WAVE_OUTPUT_FILENAME, CHANNELS, RATE, 2, f.frames)
+    multi.save_wav('speaker.wav', CHANNELS, RATE, 2, f.frames)
+    multi.save_wav('mic.wav', CHANNELS, RATE, 2, f1.frames)
 
 if __name__ == '__main__':
     main()
--- audio/server.py
+++ audio/server.py
@@ -1,8 +1,19 @@
-import pickle
 import socket
 from threading import Thread
 import time
 import multi
+import sys
+
+
+def share_audio(conn_socket, m):
+    while m.keep_going:
+        try:
+            data_to_be_sent = m.frames[0] 
+            # print(sys.getsizeof(data_to_be_sent))
+            conn_socket.send(data_to_be_sent)
+            m.frames.pop(0)
+        except IndexError:
+            pass
 
 def main():
     host = socket.gethostbyname(socket.gethostname())
@@ -16,37 +27,28 @@ def main():
     s.listen()
     print('Listening for connections...')
 
-    conn_socket, client = s.accept()
-    print(f'Connected with {client}')
-    print(f'Sending stream...')
-
-    # for mic stream
+    # for speaker loopback 
     m = multi.manager()
     m_to_store = multi.manager()
 
-    # for speaker loopback
+    # for mic stream
     m1 = multi.manager()
     m1_to_store = multi.manager()
 
     # start recording
     t = Thread(target=multi.main, args=(m,m_to_store, m1, m1_to_store))
     t.start()
-
-    # # give 3 seconds for the buffer to fill up
-    # time.sleep(0.1)
-
-    print('Starting to sending audio stream...')
-    while m.keep_going:
-        try:
-            data_to_be_sent = {
-                    'speaker':  m.frames[0], 
-                    'mic':      m1.frames[0]
-                    }
-            conn_socket.send(pickle.dumps(data_to_be_sent))
-            m.frames.pop(0)
-            m1.frames.pop(0)
-        except IndexError:
-            pass
+    i=0
+    managers = [m, m1]
+    while i<2:
+        soc, client = s.accept()
+        print(f'Connected with {client}.')
+        # # give 3 seconds for the buffer to fill up
+        # time.sleep(4)
+        t1 = Thread(target=share_audio, args=(soc, managers[i]))
+        t1.start()
+        i+=1
+    t1.join()
 
 if __name__ == '__main__':
     main()
