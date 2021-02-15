c44d1dc661c6d005270ebe320eda626a3221057a
Devin Garg <garg.7@iitj.ac.in>
2021-02-16 01:04:25 +0530

    speaker-only service added

--- audio/multi.py
+++ audio/multi.py
@@ -29,39 +29,28 @@ def save_wav(filename, channels, rate, sample_size, frames):
     wf.close()
     print(f'Saved {filename}.')
 
-def main(m=manager(), m_to_store=manager(),
-            m1=manager(), m1_to_store=manager()):
+def main(m=manager(), m_to_store=manager()):
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 2
-    RATE = 44100
-    MIC_OUTPUT_FILENAME = "mic_output.wav"
-    SPEAKER_OUTPUT_FILENAME = "speaker_output.wav"
+    RATE = 48000
+    SPEAKER_OUTPUT_FILENAME = "server_speaker.wav"
 
     p = pyaudio.PyAudio()
 
     loopback_speaker = None
 
-    print("\n********Input Devices***********")
+    # print("\n********Input Devices***********")
     for i in range(p.get_device_count()):
         dev = p.get_device_info_by_index(i)
         if dev['maxInputChannels']>0:
-            print(dev['index'], dev['name'])
+            # print(dev['index'], dev['name'])
+            pass
         if 'Stereo Mix' in dev['name']:
             loopback_speaker = int(dev['index'])
-    print("********************************\n")
-    mic = int(input("Mic Stream #"))
+    # print("********************************\n")
 
     # sys.exit()
-
-    # open mic input stream
-    stream_mic = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    input_device_index=mic,
-                    frames_per_buffer=CHUNK)
-
     # open speaker loopback stream
     stream_loopback_speaker = p.open(format=FORMAT,
                         channels=CHANNELS,
@@ -74,8 +63,6 @@ def main(m=manager(), m_to_store=manager(),
     print('Recording...')
 
     t = threading.Thread(target=recordData, args=(m, m_to_store, CHUNK, stream_loopback_speaker, ))
-    t1 = threading.Thread(target=recordData, args=(m1, m1_to_store, CHUNK, stream_mic, ))
-    t1.start()
     t.start()
 
     # start capturing speaker output into 'frames'
@@ -83,20 +70,15 @@ def main(m=manager(), m_to_store=manager(),
         if input()=='stop':
             #stop speaker loopback recording
             m.keep_going = False
-            #stop mic recording
-            m1.keep_going = False
             time.sleep(0.1)
             break
 
     # stop capturing, close the stream and terminate pyaudio
     stream_loopback_speaker.stop_stream()
     stream_loopback_speaker.close()
-    stream_mic.stop_stream()
-    stream_mic.close()
     p.terminate()
     print('Stopped recording.')
     save_wav(SPEAKER_OUTPUT_FILENAME, CHANNELS, RATE, p.get_sample_size(FORMAT), m_to_store.frames)
-    save_wav(MIC_OUTPUT_FILENAME, CHANNELS, RATE, p.get_sample_size(FORMAT), m1_to_store.frames)
 
 if __name__ == '__main__':
     main()
